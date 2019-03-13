0923b228f91942de4e00fba0bf18a751c528787e
gaojie <873037900@qq.com>
2019-03-13 17:42:37 +0800

    添加eureka注册中心

--- msa-weather-collection-server/src/main/java/com/weather/collection/CollectionApplication.java
+++ msa-weather-collection-server/src/main/java/com/weather/collection/CollectionApplication.java
@@ -2,8 +2,10 @@ package com.weather.collection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CollectionApplication {
 
     public static void main(String[] args) {
--- msa-weather-data-server/src/main/java/com/weather/data/DataApplication.java
+++ msa-weather-data-server/src/main/java/com/weather/data/DataApplication.java
@@ -2,8 +2,10 @@ package com.weather.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DataApplication {
 
     public static void main(String[] args) {
